74631b74de52ab3dd74fd6bddb7cf0bd409c6707
rutikmj712 <rutikmj7122@gmail.com>
2024-06-14 00:58:40 +0530

    welcome message

--- src/main/java/com/example/runners/RunnersApplication.java
+++ src/main/java/com/example/runners/RunnersApplication.java
@@ -2,13 +2,21 @@ package com.example.runners;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RunnersApplication {
 
 	public static void main(String[] args) {
+		/* ConfigurableApplicationContext con= SpringApplication.run(RunnersApplication.class, args);
+		//i am trying test my g
+		var Welcome=new WelcomeMessage();
+		System.out.println(Welcome.getGetWelocome());
+		WelcomeMessage welcomeMessage=(WelcomeMessage)con.getBean("welcomeMessage");
+		System.out.println(welcomeMessage.getGetWelocome()); */ 
 		SpringApplication.run(RunnersApplication.class, args);
-		//i am trying test my git hub account
+		var Welcome=new WelcomeMessage();
+		System.out.println(Welcome.getGetWelocome());
 	}
 
 }
---
+++ src/main/java/com/example/runners/WelcomeMessage.java
@@ -0,0 +1,12 @@
+package com.example.runners;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMessage {
+ 
+
+public String getGetWelocome() {
+	return "Welcome to spring boot application" ;
+}
+}
