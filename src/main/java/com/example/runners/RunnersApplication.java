e4b46b70c99fb24d53d7efe92751cc1585770a16
rutikmj712 <rutikmj7122@gmail.com>
2024-06-25 20:22:16 +0530

    logger meassage printed in application

--- src/main/java/com/example/runners/RunnersApplication.java
+++ src/main/java/com/example/runners/RunnersApplication.java
@@ -1,11 +1,15 @@
 package com.example.runners;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import ch.qos.logback.classic.Logger;
+
 @SpringBootApplication
 public class RunnersApplication {
+	private static final Logger log=(Logger) LoggerFactory.getLogger(RunnersApplication.class);
 
 	public static void main(String[] args) {
 		/* ConfigurableApplicationContext con= SpringApplication.run(RunnersApplication.class, args);
@@ -16,6 +20,7 @@ public class RunnersApplication {
 		System.out.println(welcomeMessage.getGetWelocome()); */ 
 		SpringApplication.run(RunnersApplication.class, args);
 		var Welcome=new WelcomeMessage();
+		log.info("Application started succeddfully");
 		System.out.println(Welcome.getGetWelocome());
 	}
 
